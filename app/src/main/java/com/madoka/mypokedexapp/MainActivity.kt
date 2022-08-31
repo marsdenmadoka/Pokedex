d65b861fe8a748cb140fe54d0988979ca879ccec
marsdenmadoka <marsmadoka98@gmail.com>
2022-08-31 18:33:52 +0300

    added search bar and logo

--- app/src/main/java/com/madoka/mypokedexapp/MainActivity.kt
+++ app/src/main/java/com/madoka/mypokedexapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.madoka.mypokedexapp.pokemonlist.PokemonListScreen
 import com.madoka.mypokedexapp.ui.theme.MyPokedexAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen"
                 ) {
                     composable("pokemon_list_screen") {
-
+                       PokemonListScreen(navController = navController)
                     }
                     composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
                         arguments = listOf(
